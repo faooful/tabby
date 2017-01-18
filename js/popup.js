b49e889dcbe87c499cc81b580237ab22b7ab54f1
James Brill <jebssnooker@hotmail.com>
2017-01-18 21:31:11 +0000

    Remove random code presumably from sample

--- js/popup.js
+++ js/popup.js
@@ -1,32 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        (function () {
-            var ln = links[i];
-            var location = ln.href;
-            ln.onclick = function () {
-                chrome.tabs.create({active: true, url: location});
-            };
-        })();
-    }
+$(document).ready(function() {
+  $('#home').click(function() {
+    console.log('something happened')
+    chrome.tabs.create({ url: '/home.html' })
+  });
 });
-
-_.templateSettings.variable = "element";
-var tpl = _.template($("#alertTemplate").html());
-var info = 'success';
-var warning = 'warning';
-var error = "error";
-
-$(".addAlert").on("click", function (e) {
-    e.preventDefault();
-    addAlert(info, "Alert Message");
-});
-
-function addAlert(type, message) {
-    var tplData = {
-        message: message,
-        type: type
-
-    };
-    $("#mainContainer").append(tpl(tplData));
-}
