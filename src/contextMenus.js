442ded267bc20033c8d839813e69abd799ff994c
James Brill <jebssnooker@hotmail.com>
2017-02-02 22:05:27 +0000

    On context menu interaction, send image URL to the home tab in a message

--- src/contextMenus.js
+++ src/contextMenus.js
@@ -2,6 +2,12 @@ chrome.contextMenus.create({
   title: 'Use URL of image somehow',
   contexts: ['image'],
   onclick: function(info) {
-    alert(info.srcUrl)
+    chrome.tabs.query({
+      url: 'chrome-extension://*/home.html'
+    }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        url: info.srcUrl
+      });
+    });
   }
 });
