89d2e76d2067af8a3c4624c189ac16f23fefc05c
faooful <faooful@gmail.com>
2017-02-07 13:07:39 +0000

    building component for board listings

--- src/home/components/HomePage.js
+++ src/home/components/HomePage.js
@@ -1,14 +1,24 @@
 import React, { PureComponent } from 'react'
 import Moodboard from 'src/home/components/Moodboard.js'
+import MoodboardPreview from 'src/home/components/MoodboardPreview.js'
 
 import styles from 'src/home/components/HomePage.css'
 
 export default class HomePage extends PureComponent {
+  renderPreview(title) {
+    return (
+      <MoodboardPreview className={styles.moodboardPreview} title={title} />
+    )
+  }
   render() {
+    const previewList = [ 'Exploration board', 'Icon inspiration' ]
     return (
       <div className={styles.HomePage}>
         <div className={styles.boardsPanel}>
           <p className={styles.subHeading}>BOARDS</p>
+          <div className={styles.previewList}>
+            {previewList.map(this.renderPreview)}
+          </div>
         </div>
         <div className={styles.moodboardContainer}>
           <Moodboard className={styles.currentMoodboard} />
