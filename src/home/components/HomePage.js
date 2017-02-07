7394bd4305adac1ed48078bc0fa79639045a8af2
faooful <faooful@gmail.com>
2017-02-07 18:24:38 +0000

    I broke everything

--- src/home/components/HomePage.js
+++ src/home/components/HomePage.js
@@ -7,22 +7,32 @@ import styles from 'src/home/components/HomePage.css'
 export default class HomePage extends PureComponent {
   renderPreview(title, date) {
     return (
-      <MoodboardPreview className={styles.moodboardPreview} title={title} date={date} />
+      <MoodboardPreview className={styles.moodboardPreview} photo={photo} title={title} date={date} />
     )
   }
 
   render() {
-    const previewList = {
-      'Exploration board': 'Created 13/01/16',
-      'Icon inspiration': 'Created 16/01/16'
-    }
+    const previewList = [
+      {
+        photo: 'http://lorempixel.com/400/200/',
+        title: 'Exploration board',
+        date: '13/01/2016' // TODO this should be an actual timestamp
+
+      },
+      {
+        photo: 'http://lorempixel.com/400/200/',
+        title: 'Icon inspiration',
+        date: '16/01/16'
+      }
+    ]
+
     return (
       <div className={styles.HomePage}>
         <div className={styles.boardsPanel}>
           <p className={styles.subHeading}>BOARDS</p>
           <div className={styles.previewList}>
-            {Object.keys(previewList).map((key, i) => {
-              return this.renderPreview(key, previewList[key])
+            {previewList.map((item, i) => {
+              return this.renderPreview(item.photo, item.title, item.date)
             })}
 
           </div>
