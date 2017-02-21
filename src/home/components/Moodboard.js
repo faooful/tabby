bd6e2d8b1ece1384c6e6fa8bd22562abfe390ea4
James Brill <jebssnooker@hotmail.com>
2017-02-21 21:36:06 +0000

    Add event handlers to enable image dragging

--- src/home/components/Moodboard.js
+++ src/home/components/Moodboard.js
@@ -1,5 +1,6 @@
 import React, { PureComponent, PropTypes } from 'react'
 import classNames from 'classnames'
+import { autobind } from 'core-decorators'
 
 import styles from 'src/home/components/Moodboard.css'
 
@@ -8,15 +9,41 @@ export default class Moodboard extends PureComponent {
     className: PropTypes.string
   };
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      image: null,
+      imageX: 0,
+      imageY: 0,
+      imageWidth: 0,
+      imageHeight: 0,
+      dragging: false
+    }
+  }
+
   componentWillMount() {
     chrome.extension.onMessage.addListener(message => {
       this.drawImageOnCanvas(message.url)
     })
   }
 
+  componentDidMount() {
+    this.canvas.addEventListener('mousemove', this.handleMouseMove, false)
+    this.canvas.addEventListener('mousedown', this.handleMouseDown, false)
+    this.canvas.addEventListener('mouseup', this.handleMouseUp, false)
+  }
+
+  getContext() {
+    return this.canvas.getContext('2d')
+  }
+
   drawImageOnCanvas(imageUrl) {
-    const context = this.canvas.getContext('2d')
+    const context = this.getContext()
+    const setDimensions = (imageWidth, imageHeight) =>
+      this.setState({ imageWidth, imageHeight })
     const image = new Image()
+    this.setState({ image })
     image.onload = function () {
       const maxLength = 400
       const reductionFactor = Math.max((this.width > this.height) ?
@@ -24,9 +51,35 @@ export default class Moodboard extends PureComponent {
         this.height / maxLength, 1)
       const width = this.width / reductionFactor
       const height = this.height / reductionFactor
+      setDimensions(width, height)
       context.drawImage(image, 0, 0, width, height)
     }
     image.src = imageUrl
+  }
+
+  @autobind
+  handleMouseMove(e) {
+    const context = this.getContext()
+    const { image, imageWidth, imageHeight } = this.state
+    if (this.state.dragging) {
+      this.setState({ imageX: e.offsetX, imageY: e.offsetY })
+      context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+      context.drawImage(image, e.offsetX, e.offsetY, imageWidth, imageHeight)
+    }
+  }
+
+  @autobind
+  handleMouseDown(e) {
+    const { imageX, imageY, imageWidth, imageHeight } = this.state
+    if (e.offsetX >= imageX && e.offsetX <= imageX + imageWidth &&
+        e.offsetY >= imageY && e.offsetY <= imageY + imageHeight) {
+      this.setState({ dragging: true })
+    }
+  }
+
+  @autobind
+  handleMouseUp() {
+    this.setState({ dragging: false })
   }
 
   render() {
